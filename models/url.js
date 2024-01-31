4d3fa3260e4b195fd5d313a50cc4474862d37619
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-01-31 23:44:29 +0530

    basic project setup

---
+++ models/url.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+const urlSchema = mongoose.Schema({
+    short_Url:{
+        type: String,
+        required:true,
+        unique:true
+    
+    },
+    Redirect_Url:{
+        type :String ,
+        required: true
+    },
+    visitHistory:[{timestamp:{
+        type:number
+    }}]
+},{timestamps:true});
+
+const URL=mongoose.model("url",urlSchema);
+
+module.exports=URL;
